dc477c5ef567ef489e386670dd73c09ee3933026
michaelpalumbo <emailmichaelpalumbo@gmail.com>
2019-09-18 17:58:19 -0400

    don't need this code but oh well

---
+++ client.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from websocket import create_connection
+ws = create_connection("ws://localhost:8082")
+# print("Sending 'Hello, World'...")
+ws.send("Hello, World")
+# print "Sent"
+# print "Reeiving..."
+result =  ws.recv()
+# print "Received '%s'" % result
+# ws.close()
